865dc11932ba04b69c96edf13a7f738748000159
Benyamin Darmoni <48030406+benyamindarmoni@users.noreply.github.com>
2019-03-26 10:06:20 +0200

    Update Tree.hpp

--- Tree.hpp
+++ Tree.hpp
@@ -1,47 +1,44 @@
 //Benyamon Darmoni , Danielle Zand
-#pragma once
-
-#include <iostream>
-
-
-namespace ariel{
-   
-  class Node{
-    public:
-	int value;
-	Node *left;
-	Node *right;
-   // Node();          //constractors
-    Node(int);
+	#pragma once
 	
-};
 
-class Tree {
-  
-  Node* Troot;
+	#include <iostream>
+	
 
-  public:
-  Tree(Node);
-  Tree();
-  
-    
-    public:          //functions
-    int size();
-    int parent(int);
-    int left(int);
-    int right(int);
-    Node* insert (int);
-    int root();
-    void remove (int);
-    bool contains(int);
-    void print();
-    void HelpInsert(Node**,Node**);
-    int HelpParent(Node * ,int );
-   Node* Max(Node*);
-    void HelpDelete( Node* , int );
-    bool HelpContains(Node*,int);
-    
-    };
-    
-    
-}
+	
+
+	namespace ariel{
+	  struct Node{
+	    
+		int value;
+		Node *left;
+		Node *right;
+	};
+	
+
+	class Tree {
+	  
+	  Node* Troot;
+	
+
+	  public:
+	  Tree(Node);
+	  Tree();
+	  
+	    
+	    public:          //functions
+	    int size();
+	    int parent(int);
+	    int left(int);
+	    int right(int);
+	    Node* insert (int);
+	    int root();
+	    void remove (int);
+	    bool contains(int);
+	    void print();
+	    
+	    };
+	    
+	    
+	}
+
